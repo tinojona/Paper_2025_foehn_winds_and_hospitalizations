dfd72e01aaa35de173d9a57ad60c3fa9ebb7b64e
tinojona <wind.tino@gmail.com>
2024-10-30 12:12:48 +0100

    relocation of data to server and hisogram card

--- app/ui.R
+++ app/ui.R
@@ -1,34 +1,8 @@
+# in ui.R
 
-#
-# ### LIBRARIES ----
-# library(sf)
-# library(raster)
-# library(ggplot2)
-# library(gridExtra)
-#
-# #####
-#
-#
-# ### DATA ----
-# # coordinates for the 8 stations
-# df_stations <- data.frame(station = c("Davos", "Chur", "Altdorf", "Montana", "Visp", "Magadino", "Lugano", "Poschiavo"),
-#                           x = c("2783519", "2759489", "2690181", "2601709", "2631151", "2715480", "2717874", "2801994"),
-#                           y = c("1187459", "1193182", "1193564", "1127489", "1128024", "1113161", "1095883", "1136249"),
-#                           MDSTID = c("GR06200", "GR06001", "UR03002", "VS09802", "VS09605", "TI08001", "TI08204", "GR06804"))
-#
-# # load shapefile of regions
-# mesh <- st_read("C:/Users/tinos/Documents/Master - Climate Science/3 - Master Thesis/data/MetStatRegions/centroids/shapefiles/MedStat_csr_adjusted.shp", quiet = TRUE)
-#
-# # load shapefile of centroids
-# centroids <- st_read("C:/Users/tinos/Documents/Master - Climate Science/3 - Master Thesis/data/MetStatRegions/centroids/shapefiles/MedStat_centroids_popdensity.shp", quiet = TRUE)
-#
-# #####
-#
+library(bslib)
 
-
-
-
-ui <- page_sidebar(
+ui <- bslib::page_sidebar(
 
   # App title
   title = "Foehn as a modifier of the temperature hospitalization association",
@@ -43,13 +17,23 @@ ui <- page_sidebar(
                 selected = 8000),
 
     helpText("From experience: 8000m buffer radius shows the largest effect of foehn")
-  ),
+  ,
 
-  # Enclosed area for plot
+  # Checkbox for generating descriptive statistics
+  checkboxInput("generate_stats", label = "Should I generate descriptive statistics?", value = FALSE)),
+
+  # Main content area for plot output
   card(
-    card_header("Figure of the selected MedStat regions")
+    card_header("Figure of the selected MedStat regions"),
+    plotOutput("selected_radius_plot")  # Buffer radius plot
   ),
 
-  # save output of buffer radius for server file
-  textOutput("selected_radius")
+  # Card for descriptive statistics (histograms)
+  card(
+    card_header("Descriptive Statistics - Histograms"),
+    conditionalPanel(
+      condition = "input.generate_stats == true",  # Display only if checkbox is checked
+      plotOutput("histograms")  # Output placeholder for histograms
+    )
+  )
 )
